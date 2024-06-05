bd7ed72100f7ce540bd8e8b98bfc1fd5d209946e
Anirudh Upadhya K <64802718+AnirudhUK1999@users.noreply.github.com>
2024-06-05 17:00:54 +0530

    Test icons with png files

--- src/utility/todoStructure/todoStructureUtil.js
+++ src/utility/todoStructure/todoStructureUtil.js
@@ -32,17 +32,17 @@ export function toDoStructure(title, description, dueDate, priority) {
 
   const delIcon = document.createElement("img");
   delIcon.classList.add("icon", "del-icon");
-  delIcon.setAttribute("src", "../src/icons/delete.svg");
+  delIcon.setAttribute("src", "../src/icons/delete.png");
   toDoIconsContainer.appendChild(delIcon);
 
   const editIcon = document.createElement("img");
   editIcon.classList.add("icon", "edit-icon");
-  editIcon.setAttribute("src", "../src/icons/edit.svg");
+  editIcon.setAttribute("src", "../src/icons/edit.png");
   toDoIconsContainer.appendChild(editIcon);
 
   const openIcon = document.createElement("img");
   openIcon.classList.add("icon", "open-icon");
-  openIcon.setAttribute("src", "../src/icons/open.svg");
+  openIcon.setAttribute("src", "../src/icons/open.png");
   toDoIconsContainer.appendChild(openIcon);
 
   card.appendChild(toDoIconsContainer);
